aa448c35cdb15a1b17054995874743abca8a2326
dipanshu0919 <dipanshu0919@gmail.com>
2026-03-07 21:35:48 +0530

    update

--- modules/mail_model.py
+++ modules/mail_model.py
@@ -3,26 +3,38 @@ import smtplib
 import ssl
 import threading
 
+import resend
+
 from email.message import EmailMessage
 
 from .sendlog_model import sendlog
 
 
+# def sendmailthread(receiver, subject, message):
+#     sender = "dipanshuashokagarwal@gmail.com"
+#     password = os.environ.get("MAIL_APP_PASS")
+#     context = ssl.create_default_context()
+#     msg = EmailMessage()
+#     msg["From"] = sender
+#     msg["To"] = receiver
+#     msg["Subject"] = subject
+#     msg.set_content(message)  # handles UTF-8 automatically
+
+#     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+#         smtp.login(sender, password)
+#         smtp.send_message(msg)
+#         sendlog(f"Email sent to {receiver}")
+
 def sendmailthread(receiver, subject, message):
-    sender = "dipanshuashokagarwal@gmail.com"
-    password = os.environ.get("MAIL_APP_PASS")
-    context = ssl.create_default_context()
-    msg = EmailMessage()
-    msg["From"] = sender
-    msg["To"] = receiver
-    msg["Subject"] = subject
-    msg.set_content(message)  # handles UTF-8 automatically
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
-        smtp.login(sender, password)
-        smtp.send_message(msg)
-        sendlog(f"Email sent to {receiver}")
+    resend.api_key = os.environ.get("RESEND_API_KEY")
 
+    r = resend.Emails.send({
+      "from": "SahyogSutra Support <support@sahyogsutra.run.place>",
+      "to": str(receiver),
+      "subject": str(subject),
+      "html": f"<p>{message}</p>"
+    })
 
 
 def sendmail(receiver, subject, message):
